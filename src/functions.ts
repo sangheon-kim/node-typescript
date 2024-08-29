4447e6c006eed093cf60f9809c9f53e83d7cda70
sangheon-kim <ksj83671@gmail.com>
2024-08-29 09:38:12 +0900

    [김상헌] 모듈화 설명

---
+++ src/functions.ts
@@ -0,0 +1,9 @@
+// case 1
+export const add = (a: number, b: number) => a + b;
+
+// case 2
+export default class Functions {
+  static add(a: number, b: number) {
+    return a + b;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+import fs from "fs";
+// case 1 (class)
+import Functions from "./functions";
+import * as function2 from "./functions";
+// import * as tomato from "./functions";
+
+// fs.readFile("file.txt", )
+// fs.readFileSync()
+function2.add(1, 2);
+Functions.add(1, 2);
+
+// tomato.add(1, 2);
+// tomato.Functions.add(2, 3);
